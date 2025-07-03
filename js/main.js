05bcc3ad8ac0f259ed2ffe734f3a2e4ca3597a35
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-03 23:58:52 +0600

    Faq Section

--- js/main.js
+++ js/main.js
@@ -27,3 +27,20 @@ const swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+// faq & answer
+const faqItems = document.querySelectorAll(".faq__item");
+
+faqItems.forEach((item) => {
+  const faqBtn = item.querySelector(".faq__btn");
+
+  item.addEventListener("click", () => {
+    const isOpen = item.classList.toggle("open");
+
+    const iconClass = isOpen ? "ri-subtract-fill" : "ri-add-fill";
+
+    const iconElement = faqBtn.querySelector("i");
+
+    iconElement.classList = `${iconClass} text-2xl`;
+  });
+});
