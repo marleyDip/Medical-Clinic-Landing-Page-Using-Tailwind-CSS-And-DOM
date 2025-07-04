d1014aa80226b5e79ab6e9857807c46a66bf2756
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-04 22:25:30 +0600

    Scroll Reveal Animation

--- js/main.js
+++ js/main.js
@@ -109,3 +109,102 @@ document.querySelectorAll(".fullDate").forEach((el) => {
 
 // footer year
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// scroll reveal animations
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 3000,
+  //reset: true, // reset animation on scroll
+});
+
+// hero section
+sr.reveal(".hero__text", {
+  origin: "top",
+});
+
+sr.reveal(".hero__img");
+
+// stats section
+sr.reveal(".stats__item", {
+  origin: "top",
+  distance: "100px",
+  delay: 600,
+  interval: 100,
+});
+
+// services section
+sr.reveal(".services");
+sr.reveal(".services__top");
+sr.reveal(".services__item", {
+  origin: "bottom",
+  distance: "100px",
+  delay: 600,
+  interval: 100,
+});
+
+// appointment section
+sr.reveal(".appointment__title");
+sr.reveal(".appointment__form");
+
+// testimonials section
+sr.reveal(".testimonial");
+sr.reveal(".testimonial__container");
+
+// team section
+sr.reveal(".team__title");
+sr.reveal(".team__slider");
+
+// faq section
+sr.reveal(".faq__tile");
+sr.reveal(".faq__item", {
+  origin: "bottom",
+  distance: "100px",
+  delay: 600,
+  interval: 100,
+});
+
+// departments section
+sr.reveal(".departments__bg");
+sr.reveal(".departments__container");
+
+// blog section
+sr.reveal(".blog__title");
+sr.reveal(".blog__post", {
+  origin: "bottom",
+  distance: "100px",
+  delay: 400,
+  interval: 100,
+});
+
+// brans section
+sr.reveal("brands__logo", {
+  origin: "bottom",
+  distance: "100px",
+  interval: 100,
+  delay: 600,
+});
+
+// newsletter section
+sr.reveal(".newsletter");
+sr.reveal(".newsletter__container");
+
+// footer section
+sr.reveal(".footer__item", {
+  origin: "bottom",
+  distance: "100px",
+  interval: 100,
+  delay: 600,
+  duration: 800,
+  easing: "ease-in-out",
+  opacity: 0.2,
+  scale: 0.9,
+});
+
+sr.reveal(".footer__copyright", {
+  origin: "top",
+  distance: "40px",
+  duration: 600,
+  delay: 900,
+  easing: "ease-out",
+});
